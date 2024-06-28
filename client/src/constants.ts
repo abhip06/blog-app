fe6f5f8ed97567f178189a601239c13b9e79a1ae
Abhinav Patel <abhipatel1806@gmail.com>
2024-06-28 13:06:15 +0530

    Server Origin setup

--- client/src/constants.ts
+++ client/src/constants.ts
@@ -1,3 +1,6 @@
 // All Environment variables
 
-export const SERVER_ORIGIN = import.meta.env.VITE_SERVER_ORIGIN;
+// Checking for development or production
+const development: boolean = !import.meta.env.MODE || import.meta.env.MODE === 'development';
+
+export const SERVER_ORIGIN = development ? "http://localhost:8000" : import.meta.env.VITE_SERVER_ORIGIN;
