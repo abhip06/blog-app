b35151c24ea97476c2b6776be282b0bce2da989a
Abhinav Patel <abhipatel1806@gmail.com>
2024-06-28 15:17:23 +0530

    axios configuration fixes

--- client/src/Api.ts
+++ client/src/Api.ts
@@ -2,7 +2,8 @@ import axios from "axios";
 import {SERVER_ORIGIN} from "./constants";
 
 const API = axios.create({
-    baseURL: SERVER_ORIGIN
+    baseURL: SERVER_ORIGIN,
+    withCredentials: true
 });
 
 export default API;
