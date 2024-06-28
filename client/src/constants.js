4060d248af1d9a84a4bde50855d8bf2f74772e61
Abhinav Patel <abhipatel1806@gmail.com>
2024-06-28 13:10:31 +0530

    constants.js file deleted

--- client/src/constants.js
+++
@@ -1,6 +0,0 @@
-"use strict";
-// All Environment variables
-Object.defineProperty(exports, "__esModule", { value: true });
-exports.SERVER_ORIGIN = void 0;
-exports.SERVER_ORIGIN = import.meta.env.VITE_SERVER_ORIGIN;
-console.log(exports.SERVER_ORIGIN);
