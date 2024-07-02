c3f8496d17070bc04b6516626551a4e52afa5037
Abhinav Patel <abhipatel1806@gmail.com>
2024-07-02 17:28:35 +0530

    proxy added

--- client/vite.config.ts
+++ client/vite.config.ts
@@ -1,17 +1,22 @@
-import { defineConfig } from 'vite'
+import { defineConfig, loadEnv } from 'vite'
 import react from '@vitejs/plugin-react'
 
-
 // https://vitejs.dev/config/
-export default defineConfig({
+export default ({ mode }: any) => {
+  process.env = { ...process.env, ...loadEnv(mode, process.cwd()) };
 
-  plugins: [react()],
-  // server: {
-  //   proxy: {
-  //     "/api": {
-  //       target: "http://localhost:8000",
-  //       changeOrigin: true,
-  //     }
-  //   }
-  // },
-});
+  const development: boolean = !process.env.MODE || process.env.MODE === 'development';
+  const SERVER_ORIGIN = development ? "http://localhost:8000" : process.env.VITE_SERVER_ORIGIN;
+
+  return defineConfig({
+    plugins: [react()],
+    server: {
+      proxy: {
+        "/api": {
+          target: SERVER_ORIGIN,
+          changeOrigin: true,
+        }
+      }
+    },
+  })
+}
