7f79213bebe84d9951fea028bc07b5ff8bfbaf7e
Abhinav Patel <abhipatel1806@gmail.com>
2024-07-02 18:04:12 +0530

    vite.config.ts file modified

--- client/vite.config.ts
+++ client/vite.config.ts
@@ -5,14 +5,12 @@ import react from '@vitejs/plugin-react'
 export default ({ mode }: any) => {
   process.env = { ...process.env, ...loadEnv(mode, process.cwd()) };
 
-  const SERVER_ORIGIN = process.env.PROD ? process.env.VITE_SERVER_ORIGIN : "http://localhost:8000";
-
   return defineConfig({
     plugins: [react()],
     server: {
       proxy: {
         "/api": {
-          target: SERVER_ORIGIN,
+          target: process.env.VITE_SERVER_ORIGIN,
           changeOrigin: true,
         }
       }
