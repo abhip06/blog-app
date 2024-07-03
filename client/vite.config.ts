a8370230dc7e2d2a4a557a92a4af14942b15577c
Abhinav Patel <abhipatel1806@gmail.com>
2024-07-03 13:19:16 +0530

    vite.config.ts file modified

--- client/vite.config.ts
+++ client/vite.config.ts
@@ -2,19 +2,14 @@ import { defineConfig, loadEnv } from 'vite'
 import react from '@vitejs/plugin-react'
 
 // https://vitejs.dev/config/
-export default ({ mode }: any) => {
-  process.env = { ...process.env, ...loadEnv(mode, process.cwd()) };
-
-  return defineConfig({
+export default defineConfig({
     plugins: [react()],
     server: {
       proxy: {
         "/api": {
-          target: process.env.VITE_SERVER_ORIGIN,
+          target: "https://blog-app-dw5w.onrender.com",
           changeOrigin: true,
-          rewrite: (path) => path.replace(/^\/api/, ''),
         }
       }
     },
-  })
-}
+  });
